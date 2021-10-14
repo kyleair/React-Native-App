4e96a49427a6538571439c445eb7a4fd542ee1fe
Kyle <kyleair8@gmail.com>
2021-10-14 12:21:36 -0400

    added plus signs in front of positive odds and added best odds calculation

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, ScrollView } from 'react-native';
 import goodd from './data2.json';
 
 
@@ -14,35 +14,35 @@ export default class App extends Component {
 }
 
 componentDidMount() {
-  fetch("https://odds.p.rapidapi.com/v1/odds?sport=americanfootball_nfl&region=us&mkt=h2h&dateFormat=iso&oddsFormat=american", {
-    "method": "GET",
-    "headers": {
-      "x-rapidapi-host": "odds.p.rapidapi.com",
-      "x-rapidapi-key": "86a584335bmshc19523db0e51bbfp11a759jsnb08a99cb55ba"
-    }
-  })
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            databruh: result.data
-          });
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
-//  this.setState({
-//    isLoaded: true,
-//    databruh: goodd.data
-//  })
+  // fetch("https://odds.p.rapidapi.com/v1/odds?sport=americanfootball_nfl&region=us&mkt=h2h&dateFormat=iso&oddsFormat=american", {
+  //   "method": "GET",
+  //   "headers": {
+  //     "x-rapidapi-host": "odds.p.rapidapi.com",
+  //     "x-rapidapi-key": "86a584335bmshc19523db0e51bbfp11a759jsnb08a99cb55ba"
+  //   }
+  // })
+  //     .then(res => res.json())
+  //     .then(
+  //       (result) => {
+  //         this.setState({
+  //           isLoaded: true,
+  //           databruh: result.data
+  //         });
+  //       },
+  //       // Note: it's important to handle errors here
+  //       // instead of a catch() block so that we don't swallow
+  //       // exceptions from actual bugs in components.
+  //       (error) => {
+  //         this.setState({
+  //           isLoaded: true,
+  //           error
+  //         });
+  //       }
+  //     )
+ this.setState({
+   isLoaded: true,
+   databruh: goodd.data
+ })
 }
 render(){
   const { error, isLoaded, databruh } = this.state;
@@ -52,17 +52,28 @@ render(){
       return <SafeAreaView><Text>Loading...</Text></SafeAreaView>;
     } else {
       return (
-        // <SafeAreaView>
-        //   {databruh[0].teams.map((team) => {
-        //     return(<Text>{team}</Text>)
-        //   })} 
-        // </SafeAreaView>
         <SafeAreaView>
+          <ScrollView>
           {databruh.map((game) => {
+            let avg_odds_zero=0
+            let best_odds_zero=game.sites[0].odds.h2h[0]
+            let avg_odds_one=0
+            let best_odds_one=game.sites[0].odds.h2h[1]
            return(<Text>
-              {game.teams[0]} ({game.sites[0].odds.h2h[0]}) vs {game.teams[1]}{'\n'}
+              {game.teams[0]} vs {game.teams[1]} {'\n'}
+              {game.sites.map((site) => {
+                if (site.odds.h2h[0] > best_odds_zero){
+                  best_odds_zero = site.odds.h2h[0]
+                }
+                if (site.odds.h2h[1] > best_odds_one){
+                  best_odds_one = site.odds.h2h[1]
+                }
+
+              })}
+              (Avg:  {(game.sites[0].odds.h2h[0]<=0?"":"+")}{game.sites[0].odds.h2h[0]} | Best: {(best_odds_zero<=0?"":"+")}{best_odds_zero}) vs (Avg: 300| Best: {(best_odds_one<=0?"":"+")}{best_odds_one}) {'\n'}
               </Text>) 
           })}
+          </ScrollView>
         </SafeAreaView>
       );
     }
